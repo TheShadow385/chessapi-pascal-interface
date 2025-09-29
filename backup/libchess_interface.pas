f0743faadbb4a436f9595bfbf07a97e413b7d7fa
TheShadow385 <justin-2000@gmx.de>
2025-09-29 16:46:40 +0200

    working interface
    switched to x64
    working "first move" bot

--- backup/libchess_interface.pas
+++ backup/libchess_interface.pas
@@ -4,47 +4,52 @@ unit libchess_interface;
 
 interface
 
+uses
+   ctypes;
 
 type
   PBoard = Pointer;
-  PMove = Pointer;
-  PInt = Pointer;
-  BitBoard = QWord;
+  BitBoard = cuint64;
 
-type
    //! Player color
    PlayerColor = (WHITE,BLACK);
 
    //! Piece type
    PieceType = (
-       PAWN = 1, // A pawn piece
-       BISHOP,   // A bishop piece
-       KNIGHT,   // A knight piece
-       ROOK,     // A rook piece
-       QUEEN,    // A queen piece
-       KING      // A king piece
+       PAWN   = 1, // A pawn piece
+       BISHOP = 2, // A bishop piece
+       KNIGHT = 3, // A knight piece
+       ROOK   = 4, // A rook piece
+       QUEEN  = 5, // A queen piece
+       KING   = 6  // A king piece
    );
 
    //! Game play state
    GameState = (
        GAME_CHECKMATE = -1, // Indicates the game has ended in a checkmate
-       GAME_NORMAL,         // Indicates the game has not ended yet
-       GAME_STALEMATE       // Indicates the game has ended in a draw
+       GAME_NORMAL    = +0, // Indicates the game has not ended yet
+       GAME_STALEMATE = +1  // Indicates the game has ended in a draw
    );
 
   //! A Move represents a single chess move from a start location to an end location
   TMove = packed record
       from_move:BitBoard;      // A BitBoard representing the origin of the move
       to_move:BitBoard;        // A BitBorad representing the target of the move
-      promotion:Byte;     // Will be one of the BISHOP, KNIGHT, ROOK, QUEEN constants above, or 0 if not required
-      capture:boolean;    // True if this move captures a piece
-      castle:boolean;     // True if this move is castling
+      promotion:cuint8;     // Will be one of the BISHOP, KNIGHT, ROOK, QUEEN constants above, or 0 if not required
+      capture:cbool;    // True if this move captures a piece
+      castle:cbool;     // True if this move is castling
   end;
+  PMove = ^TMove;
+
+  //ary_moves = array of TMove;
 
 
-function chess_get_board:PBoard;                                                cdecl; external 'libchess.dll';
+function chess_get_board():PBoard;                                              cdecl; external 'libchess.dll';
 function chess_clone_board(Board:PBoard):PBoard;                                cdecl; external 'libchess.dll';
-function chess_get_legal_moves(Board:PBoard; len:PInt):PMove; {Array}           cdecl; external 'libchess.dll';
+function chess_get_legal_moves(Board:PBoard; len:Pcint):PMove;                  cdecl; external 'libchess.dll';
+function chess_get_legal_moves_inplace(board: PBoard; moves:PMove;
+                                       maxlen_moves:csize_t):cint;              cdecl; external 'libchess.dll';
+
 
 function chess_is_white_turn(Board: PBoard):boolean;                            cdecl; external 'libchess.dll';
 function chess_is_black_turn(Board: PBoard):boolean;                            cdecl; external 'libchess.dll';
@@ -78,6 +83,8 @@ function chess_get_half_moves(Board: PBoard):LongInt;                           
 
 procedure chess_push(move: TMove);                                              cdecl; external 'libchess.dll';
 procedure chess_done;                                                           cdecl; external 'libchess.dll';
+function chess_board_from_fen(fen:PChar):PBoard;                                cdecl; external 'libchess.dll';
+procedure chess_dump_move(buffer:PChar; move:TMove);                            cdecl; external 'libchess.dll';
 
 ///// TIME MANAGEMENT /////
 
@@ -99,8 +106,8 @@ function chess_get_bitboard_from_index(index:LongInt):BitBoard;                 
 
 ///// OTHER /////
 
-function chess_get_opponent_move(index:LongInt):BitBoard;                       cdecl; external 'libchess.dll';
-function chess_free_moves_array(moves:PMove):BitBoard;                          cdecl; external 'libchess.dll';
+function chess_get_opponent_move(index:LongInt):TMove;                          cdecl; external 'libchess.dll';
+procedure chess_free_moves_array(moves:PMove);                                  cdecl; external 'libchess.dll';
 
 function hello:boolean; cdecl; external 'libchess.dll';
 
--- chess_interface.lpr
+++ chess_interface.lpr
@@ -1,24 +1,43 @@
 program chess_interface;
 
 uses
-  Classes, SysUtils, Dialogs, libchess_interface;
+  Classes, SysUtils, Dialogs, libchess_interface, ctypes;
 
 var
    i:integer;
    CurrentBoard:PBoard;
-   Moves:PMove;
-   len:LongInt;
+   AllMoves:PMove;
+
+   moveCount: cint;
+   buf: array[0..7] of Char; // must be at least 7 bytes as per docs
 
 begin
+   WriteLn('started');
+
    for i:=0 to 500 do
    begin
-      CurrentBoard:=chess_get_board;
+      try
+         try
+            CurrentBoard:=chess_get_board();
+         except
+            WriteLn('Error getting board');
+         end;
+      finally
+        WriteLn('got board');
+      end;
 
-      chess_get_legal_moves(CurrentBoard,len);
-      chess_push(moves[Random(25555) mod len]);
 
+      AllMoves:=chess_get_legal_moves(CurrentBoard,@moveCount);
+
+      if moveCount>0 then chess_push(AllMoves[0]);
+
+      chess_free_moves_array(AllMoves);
       chess_free_board(CurrentBoard);
       chess_done();
    end;
+
+   WriteLn('done');
+
 end.
+
 
