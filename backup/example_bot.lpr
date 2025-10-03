b200cf611fc71478095599ec4ff5debaac9e5156
TheShadow385 <justin-2000@gmx.de>
2025-10-03 13:44:08 +0200

    1.0 commit
    removing extra code
    changing some variable types
    removing move display code from example

--- backup/example_bot.lpr
+++ backup/example_bot.lpr
@@ -3,59 +3,25 @@ program example_bot;
 {$mode objfpc}{$H+}
 
 uses
-  {$IFDEF WINDOWS}
-  Windows,
-  {$ENDIF}
   {$IFDEF UNIX}
   cthreads,
   {$ENDIF}
   TypInfo, SysUtils, Classes, libchess_interface, ctypes;
 
 var
-   i,j:integer;
+   i:integer;
    CurrentBoard:PBoard=nil;
    AllMoves:PMove=nil;
    moveCount:cint=0;
 
-function BitboardToSquare(b: QWord): String;
-const
-  Files = 'abcdefgh';
-var
-  idx, fileIdx, rankIdx: Integer;
-begin
-  if b = 0 then
-    Exit(''); // no piece
-
-  // find index of the 1-bit
-  idx := 0;
-  while (b and 1) = 0 do begin
-    b := b shr 1;
-    Inc(idx);
-  end;
-
-  fileIdx := idx mod 8;
-  rankIdx := idx div 8;
-
-  Result := Files[fileIdx+1] + IntToStr(rankIdx+1);
-end;
-
 begin
    Randomize;
-   SetEnvironmentVariable('CHESS_BOT_NAME','FREEPASCAL_BOT');
 
    for i:=0 to 500 do
    begin
       CurrentBoard:=chess_get_board();
 
       AllMoves:=chess_get_legal_moves(CurrentBoard,@moveCount);
-
-      for j:=0 to moveCount-1 do
-      begin
-         DebugMsg(
-                  BitboardToSquare(AllMoves[j].from_move)+'-'+BitboardToSquare(AllMoves[j].to_move)
-                  +' cap:'+AllMoves[j].capture.ToString+' cas:'+AllMoves[j].castle.ToString+' pro:'+AllMoves[j].promotion.ToString
-                  );
-      end;
 
       if moveCount>0 then chess_push(AllMoves[Random(moveCount)]);
 
--- libchess_interface.pas
+++ libchess_interface.pas
@@ -13,8 +13,8 @@ type
 
    //! Player color
    PlayerColor = (
-       WHITE = -1,
-       BLACK = +0
+       WHITE,
+       BLACK
    );
 
    //! Piece type
@@ -38,89 +38,88 @@ type
 
   //! A Move represents a single chess move from a start location to an end location
   TMove = packed record
-      from_move:BitBoard;      // A BitBoard representing the origin of the move
-      to_move:BitBoard;        // A BitBorad representing the target of the move
-      promotion:cuint8;     // Will be one of the BISHOP, KNIGHT, ROOK, QUEEN constants above, or 0 if not required
-      capture:cuint8;    // True if this move captures a piece
-      castle:cuint8;     // True if this move is castling
+      from_move:BitBoard; // A BitBoard representing the origin of the move
+      to_move:BitBoard;   // A BitBorad representing the target of the move
+      promotion:cuint8;   // Will be one of the BISHOP, KNIGHT, ROOK, QUEEN constants above, or 0 if not required
+      capture:cuint8;     // True if this move captures a piece
+      castle:cuint8;      // True if this move is castling
 
       _padding: array[0..4] of cuint8; // manual padding
   end;
   PMove = ^TMove;
 
-function chess_get_board():PBoard;                                              cdecl; external 'libchess.dll';
-function chess_clone_board(Board:PBoard):PBoard;                                cdecl; external 'libchess.dll';
-function chess_get_legal_moves(Board:PBoard; len:Pcint):PMove;                  cdecl; external 'libchess.dll';
+  {$IFDEF WINDOWS}
+  const libchess='libchess.dll';
+  {$ENDIF}
+  {$IFDEF UNIX}
+  const libchess='libchess.so';
+  {$ENDIF}
+
+function chess_get_board():PBoard;                                              cdecl; external libchess;
+function chess_clone_board(Board:PBoard):PBoard;                                cdecl; external libchess;
+function chess_get_legal_moves(Board:PBoard; len:Pcint):PMove;                  cdecl; external libchess;
 function chess_get_legal_moves_inplace(board: PBoard; moves:PMove;
-                                       maxlen_moves:csize_t):cint;              cdecl; external 'libchess.dll';
+                                       maxlen_moves:csize_t):cint;              cdecl; external libchess;
 
-function chess_is_white_turn(Board: PBoard):boolean;                            cdecl; external 'libchess.dll';
-function chess_is_black_turn(Board: PBoard):boolean;                            cdecl; external 'libchess.dll';
+function chess_is_white_turn(Board: PBoard):boolean;                            cdecl; external libchess;
+function chess_is_black_turn(Board: PBoard):boolean;                            cdecl; external libchess;
 
-procedure chess_skip_turn(Board: PBoard);                                       cdecl; external 'libchess.dll';
+procedure chess_skip_turn(Board: PBoard);                                       cdecl; external libchess;
 
-function chess_in_check(Board: PBoard):boolean;                                 cdecl; external 'libchess.dll';
-function chess_in_checkmate(Board: PBoard):boolean;                             cdecl; external 'libchess.dll';
-function chess_in_draw(Board: PBoard):boolean;                                  cdecl; external 'libchess.dll';
+function chess_in_check(Board: PBoard):boolean;                                 cdecl; external libchess;
+function chess_in_checkmate(Board: PBoard):boolean;                             cdecl; external libchess;
+function chess_in_draw(Board: PBoard):boolean;                                  cdecl; external libchess;
 
-function chess_can_kingside_castle(Board: PBoard; color:PlayerColor):boolean;   cdecl; external 'libchess.dll';
-function chess_can_queenside_castle(Board: PBoard; color:PlayerColor):boolean;  cdecl; external 'libchess.dll';
+function chess_can_kingside_castle(Board: PBoard; color:PlayerColor):boolean;   cdecl; external libchess;
+function chess_can_queenside_castle(Board: PBoard; color:PlayerColor):boolean;  cdecl; external libchess;
 
-function chess_get_game_state(Board: PBoard):GameState;                         cdecl; external 'libchess.dll';
-function chess_is_game_ended(Board: PBoard):GameState;                          cdecl; external 'libchess.dll';
+function chess_get_game_state(Board: PBoard):GameState;                         cdecl; external libchess;
+function chess_is_game_ended(Board: PBoard):GameState;                          cdecl; external libchess;
 
-function chess_zobrist_key(Board: PBoard):QWord;                                cdecl; external 'libchess.dll';
+function chess_zobrist_key(Board: PBoard):cuint64;                              cdecl; external libchess;
 
-procedure chess_make_move(Board: PBoard; move:TMove);                           cdecl; external 'libchess.dll';
-procedure chess_undo_move(Board: PBoard);                                       cdecl; external 'libchess.dll';
+procedure chess_make_move(Board: PBoard; move:TMove);                           cdecl; external libchess;
+procedure chess_undo_move(Board: PBoard);                                       cdecl; external libchess;
 
-procedure chess_free_board(Board: PBoard);                                      cdecl; external 'libchess.dll';
+procedure chess_free_board(Board: PBoard);                                      cdecl; external libchess;
 
 function chess_get_bitboard(Board: PBoard; color:PlayerColor;
-                                           piece_type:PieceType):BitBoard;      cdecl; external 'libchess.dll';
+                                           piece_type:PieceType):BitBoard;      cdecl; external libchess;
 
-function chess_get_full_moves(Board: PBoard):LongInt;                           cdecl; external 'libchess.dll';
-function chess_get_half_moves(Board: PBoard):LongInt;                           cdecl; external 'libchess.dll';
+function chess_get_full_moves(Board: PBoard):cint;                              cdecl; external libchess;
+function chess_get_half_moves(Board: PBoard):cint;                              cdecl; external libchess;
 
 ///// MOVE SUBMISSION /////
 
-procedure chess_push(move: TMove);                                              cdecl; external 'libchess.dll';
-procedure chess_done;                                                           cdecl; external 'libchess.dll';
-function chess_board_from_fen(fen:PChar):PBoard;                                cdecl; external 'libchess.dll';
-procedure chess_dump_move(buffer:PChar; move:TMove);                            cdecl; external 'libchess.dll';
+procedure chess_push(move: TMove);                                              cdecl; external libchess;
+procedure chess_done;                                                           cdecl; external libchess;
+function chess_board_from_fen(fen:PChar):PBoard;                                cdecl; external libchess;
+procedure chess_dump_move(buffer:PChar; move:TMove);                            cdecl; external libchess;
 
 ///// TIME MANAGEMENT /////
 
-function chess_get_time_millis:QWord;                                           cdecl; external 'libchess.dll';
-function chess_get_opponent_time_millis:QWord;                                  cdecl; external 'libchess.dll';
-function chess_get_elapsed_time_millis:QWord;                                   cdecl; external 'libchess.dll';
+function chess_get_time_millis:cuint64;                                         cdecl; external libchess;
+function chess_get_opponent_time_millis:cuint64;                                cdecl; external libchess;
+function chess_get_elapsed_time_millis:cuint64;                                 cdecl; external libchess;
 
 ///// BITBOARDS /////
 
-function chess_get_piece_from_index(Board: PBoard):PieceType;                   cdecl; external 'libchess.dll';
+function chess_get_piece_from_index(Board: PBoard; index: cint):PieceType;      cdecl; external libchess;
 function chess_get_piece_from_bitboard(Board: PBoard;
-                                       Bitboard:BitBoard):PieceType;            cdecl; external 'libchess.dll';
-function chess_get_color_from_index(Board: PBoard; index:LongInt):PlayerColor;  cdecl; external 'libchess.dll';
+                                       Bitboard:BitBoard):PieceType;            cdecl; external libchess;
+function chess_get_color_from_index(Board: PBoard; index:cint):PlayerColor;     cdecl; external libchess;
 function chess_get_color_from_bitboard(Board: PBoard;
-                                       Bitboard:BitBoard):PlayerColor;          cdecl; external 'libchess.dll';
+                                       Bitboard:BitBoard):PlayerColor;          cdecl; external libchess;
 
-function chess_get_index_from_bitboard(Bitboard:BitBoard):LongInt;              cdecl; external 'libchess.dll';
-function chess_get_bitboard_from_index(index:LongInt):BitBoard;                 cdecl; external 'libchess.dll';
+function chess_get_index_from_bitboard(Bitboard:BitBoard):cint;                 cdecl; external libchess;
+function chess_get_bitboard_from_index(index:cint):BitBoard;                    cdecl; external libchess;
 
 ///// OTHER /////
 
-function chess_get_opponent_move(index:LongInt):TMove;                          cdecl; external 'libchess.dll';
-procedure chess_free_moves_array(moves:PMove);                                  cdecl; external 'libchess.dll';
-
-procedure DebugMsg(const Msg: string);
+function chess_get_opponent_move(index:LongInt):TMove;                          cdecl; external libchess;
+procedure chess_free_moves_array(moves:PMove);                                  cdecl; external libchess;
 
 implementation
-
-procedure DebugMsg(const Msg: string);
-begin
-   WriteLn('info ' + Msg);
-   Flush(Output);
-end;
 
 end.
 
